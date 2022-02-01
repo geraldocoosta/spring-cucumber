fc706597310b1e251d92bb946632f3e90969eddb
Geraldo Costa <geraldocosta@ciandt.com>
2022-01-31 21:48:06 -0300

    first commit

---
+++ src/test/java/io/tpd/springbootcucumber/bagbasics/BagCucumberIntegrationTest.java
@@ -0,0 +1,13 @@
+package io.tpd.springbootcucumber.bagbasics;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/features/bagbasics",
+        plugin = {"pretty", "html:target/cucumber/bagbasics"},
+        extraGlue = "io.tpd.springbootcucumber.bagcommons",
+        publish = true)
+public class BagCucumberIntegrationTest {
+}
---
+++ src/test/java/io/tpd/springbootcucumber/bagextra/BagExtraCucumberIntegrationTest.java
@@ -0,0 +1,13 @@
+package io.tpd.springbootcucumber.bagextra;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/features/bagextra",
+        plugin = {"pretty", "html:target/cucumber/bagextra"},
+        extraGlue = "io.tpd.springbootcucumber.bagcommons",
+        publish = true)
+public class BagExtraCucumberIntegrationTest {
+}
